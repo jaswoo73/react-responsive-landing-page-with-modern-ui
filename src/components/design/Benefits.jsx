1d8ea30935b7e3baaa06cb42a5447e2c9c39f03b
Jason Woo <jaswoo73@gmail.com>
2024-05-14 15:50:40 +0800

    First commit

---
+++ src/components/design/Benefits.jsx
@@ -0,0 +1,5 @@
+export const GradientLight = () => {
+  return (
+    <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#413a7b] to-[#911a99]/0 to-70% pointer-events-none" />
+  );
+};
